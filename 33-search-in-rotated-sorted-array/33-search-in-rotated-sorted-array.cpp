76090e33c60fea87964becb77ade5115a4a46ca7
Abhishek kumar Jha <79482823+akj1608@users.noreply.github.com>
2022-03-10 15:38:22 +0530

    Time: 4 ms (73.95%), Space: 11 MB (76.06%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int x=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target){
+                x=i;
+                
+            }
+            
+                
+        }
+        return x;
+    }
+};
