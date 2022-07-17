6e1a83f9470cde6caea50884b248382960af9063
Abhishek kumar Jha <79482823+akj1608@users.noreply.github.com>
2022-07-17 17:06:57 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.08%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    void gen(vector<int>& ds,vector<int>& nums,vector<vector<int>>& res,int freq[])
+    void gen(int i,vector<int>& nums,vector<vector<int>>& res)
     {
-        if(ds.size()==nums.size())
-        {
-            res.push_back(ds);
-            return;
-        }
-      for(int i=0;i<nums.size();i++)
+      if(i==nums.size())
       {
-          if(!freq[i])
-          {
-              ds.push_back(nums[i]);
-              freq[i]=1;
-              gen(ds,nums,res,freq);
-              freq[i]=0;
-              ds.pop_back();
-          }
+        res.push_back(nums);
+        return;
       }
-        
-     }
-    
+      
+      for(int a=i;a<nums.size();a++)
+      {
+        swap(nums[i],nums[a]);
+        gen(i+1,nums,res);
+         swap(nums[i],nums[a]);
+      }
+      
+    }
+             
     
     vector<vector<int>> permute(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ds;
-        vector<vector<int>>res;
-       int freq[n];
-        for(int i=0;i<n;i++)
-            freq[i]=0;
+      
+        vector<vector<int>> res;
+      gen(0,nums,res);
         
-        gen(ds,nums,res,freq);
-        return res;
-        
-        
+      return res;
     }
 };
