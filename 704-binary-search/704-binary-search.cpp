986ef63a40c4cfda8c982ca12781327b25a1915e
Abhishek kumar Jha <79482823+akj1608@users.noreply.github.com>
2022-07-22 14:07:40 +0530

    Time: 50 ms (66.87%), Space: 27.5 MB (63.76%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -7,24 +7,17 @@ public:
         int high=n-1;
         while(low<=high)
         {
-          int  mid=(low+high)>>1;
+          int  mid=low+(high-low)/2;
             
             if(nums[mid]==target)
             {
                 return mid;
             }
-           else if(target>=nums[low] && target<=nums[mid]){
-                high=mid-1;
-            }
-          else if(target>=nums[mid] && target<=nums[high]){
-                low=mid+1;
-            }
-            else
-            {
-                return -1;
-            }
+          if(target<nums[mid]) high=mid-1;
+          else low=mid+1;
+           
         }
-        return -1;
+       return -1;
     }   
     
 };
