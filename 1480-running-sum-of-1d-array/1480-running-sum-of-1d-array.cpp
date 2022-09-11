2d35aab324a1f516dd96eecc1ec71d5ee503d2b2
Abhishek kumar Jha <79482823+akj1608@users.noreply.github.com>
2022-09-11 18:28:05 +0530

    Time: 9 ms (19.18%), Space: 8.6 MB (37.34%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -9,9 +9,9 @@ public:
         for(int i=0;i<n;i++)
         {
             sum+=nums[i];
-            v.push_back(sum);
+            nums[i]=sum;
         }
         
-        return v;
+        return nums;
     }
 };
