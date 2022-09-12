1627082df55ba60eb7602b3f8e2330f7d3b06a70
Abhishek kumar Jha <79482823+akj1608@users.noreply.github.com>
2022-09-12 21:45:49 +0530

    Time: 849 ms (13.08%), Space: 10.2 MB (74.97%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,19 +4,19 @@ public:
         int n= arr.size(),ans=0,res=0;
         vector<int> result;
         
-        int i=0,j=n-1,k=n-1;
+        int i=0,j=n-1,k=n-1,a,b;
         
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(arr[i]+arr[j]==target)
                 {
-                    result.push_back(i);
-                     result.push_back(j);
+                   a=i;
+                  b=j;
                     break;
                     
                 }
             }
         }
-        return result;
+        return {a,b};
     }
 };
