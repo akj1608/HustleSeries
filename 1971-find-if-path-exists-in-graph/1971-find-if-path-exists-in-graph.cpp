324431df6fa8d0ea5f11828c254686fda11f6600
Abhishek kumar Jha <79482823+akj1608@users.noreply.github.com>
2022-10-07 18:47:40 +0530

    Time: 1304 ms (38.73%), Space: 225.5 MB (16.40%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-   bool has_path(vector<list<int>> &graph,int start,int end,vector<bool> &visited)
-   {
-     if(start==end) return true;
-     visited[start]=true;
-     for(auto &neighbour : graph[start])
-     {
-       if( not visited[neighbour])
-       {
-         bool result= has_path(graph,neighbour,end,visited);
-         if(result) return true;
-       }
-       
-     }
-     return false;
-   }
-     
-     
-     
-     
-     
-     
-    bool validPath(int n, vector<vector<int>>& edges, int start, int end) 
+  bool has_path(vector<list<int>> &graph,int start,int end,vector<bool> &visited)
+  {
+    if(start==end ) return true;
+    visited[start]=true;
+    
+    for(auto &neighbour : graph[start])
     {
+      if(not visited[neighbour] )
+      {
+        bool result= has_path(graph,neighbour,end,visited);
+        if(result ) return true;
+      }
+    }
+      return false;
+  }
+  
+  
+  
+  
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
       
       vector<list<int>> graph(n,list<int>());
       
       for(auto &edge : edges)
       {
         int u=edge[0];
-        int v=edge[1];
+        int v=edge [1];
         
-        graph[v].push_back(u);
+        
         graph[u].push_back(v);
+        graph[v].push_back(u);
+        
       }
       
       vector<bool> visited(n,false);
@@ -40,6 +39,4 @@ public:
       return has_path(graph,start,end,visited);
         
     }
-  
- 
 };
