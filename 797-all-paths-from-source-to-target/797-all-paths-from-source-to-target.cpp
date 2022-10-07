6ccdb94bc68764c87e5ff166b76df00f2ba5e1da
Abhishek kumar Jha <79482823+akj1608@users.noreply.github.com>
2022-10-08 01:30:06 +0530

    Time: 27 ms (51.32%), Space: 12 MB (50.47%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
+  vector<vector<int>>result;
   
-  vector<vector<int>>res;
-  
-  
-  void allpath(vector<vector<int>>& graph,int start,int end, vector<int> &paths,vector<bool> &visited)
+  void allpath(vector<vector<int>>&graph,int start,int end,vector<int>&path,vector<bool>&visited)
   {
-    
     if(start==end)
     {
-      paths.push_back(end);
-      res.push_back(paths);
-      paths.pop_back();
-      return;
+      path.push_back(end);
+      result.push_back(path);
+      path.pop_back();
+      
     }
-    paths.push_back(start);
+    path.push_back(start);
     visited[start]=true;
-    
-    for(auto &neighbour: graph[start])
+    for(auto &neighbour : graph[start])
     {
       if(not visited[neighbour])
-      {
-        allpath(graph,neighbour,end,paths,visited);
-      }
+        allpath(graph,neighbour,end,path,visited);
     }
     visited[start]=false;
-    paths.pop_back();
-    
-  }  
+    path.pop_back();
+  }
   
   
   
   
   
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-      vector<int> paths;
+        vector<int>path;
       int n=graph.size();
-      vector<bool>visited(n,false);
+      vector<bool> visited(n,false);
+      allpath(graph,0,n-1,path,visited);
       
-      allpath(graph,0,n-1,paths,visited);
-      return res;
-        
+      return result;
     }
-  
-  
 };
